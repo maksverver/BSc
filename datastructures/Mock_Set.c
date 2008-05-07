821e8814e362e1f9bbf1665682422d238f740aa2
Maks Verver <maksverver@geocities.com>
2008-05-07 23:03:42 +0200

    Changed mock set implementation so it uses a mmap()ed file while recording. This improves performance; replaying is now nearly as fast as using the dummy set, so this is good enough as a base level for comparison.

--- datastructures/Mock_Set.c
+++ datastructures/Mock_Set.c
@@ -2,23 +2,35 @@
 #include "comparison.h"
 #include <assert.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 typedef struct Mock_Set Mock_Set;
 
 struct Mock_Set
 {
     Set     base;
-    Set     *impl;      /* Actual implementation (when recording) */
-    FILE    *fp;        /* File being written to/read from */
+
+    /* When recording: */
+    Set     *impl;
+    FILE    *fp;
+
+    /* When replaying */
+    char    *begin, *end, *pos;
+    int     fd;
 };
 
 static bool set_insert(Mock_Set *set, const void *key_data, size_t key_size)
 {
-    int ch;
     bool res;
 
     if (set->impl != NULL)
     {
+        char ch;
+
         /* Use real set implementation to obtain result */
         set->impl->hash    = set->base.hash;
         set->impl->compare = set->base.compare;
@@ -27,14 +39,12 @@ static bool set_insert(Mock_Set *set, const void *key_data, size_t key_size)
         /* Write result to file */
         ch = fputc(res, set->fp);
         assert(ch == 0 || ch == 1);
-        return ch;
     }
     else
     {
         /* Read result from file */
-        ch = fgetc(set->fp);
-        assert(ch == 0 || ch == 1);
-        res = ch;
+        assert(set->pos < set->end);
+        res = *set->pos++;
     }
 
     return res;
@@ -42,25 +52,24 @@ static bool set_insert(Mock_Set *set, const void *key_data, size_t key_size)
 
 static bool set_contains(Mock_Set *set, const void *key_data, size_t key_size)
 {
-    int ch;
     bool res;
 
     if (set->impl != NULL)
     {
+        char ch;
+
         /* Use real set implementation to obtain result */
-        res = set->impl->insert(set->impl, key_data, key_size);
+        res = set->impl->contains(set->impl, key_data, key_size);
 
         /* Write result to file */
         ch = fputc(res, set->fp);
         assert(ch == 0 || ch == 1);
-        return ch;
     }
     else
     {
         /* Read result from file */
-        ch = fgetc(set->fp);
-        assert(ch == 0 || ch == 1);
-        res = ch;
+        assert(set->pos < set->end);
+        res = *set->pos++;
     }
 
     return res;
@@ -69,46 +78,72 @@ static bool set_contains(Mock_Set *set, const void *key_data, size_t key_size)
 static void set_destroy(Mock_Set *set)
 {
     if (set->impl != NULL)
-    {
         set->impl->destroy(set->impl);
-    }
-    fclose(set->fp);
+    if (set->fp != NULL)
+        fclose(set->fp);
+    if (set->begin != NULL)
+        munmap(set->begin, set->end - set->begin);
+    if (set->fd != -1)
+        close(set->fd);
     free(set);
 }
 
 Set *Mock_Set_create(const char *path, bool recording)
 {
-    Mock_Set    *set;
-    Set         *impl;
-    FILE        *fp;
+    Mock_Set *set;
 
     /* Allocate memory */
     set = malloc(sizeof(Mock_Set));
     if (set == NULL)
         return NULL;
 
-    /* Create real set instance, if recording */
+    set->impl  = NULL;
+    set->fp    = NULL;
+    set->begin = NULL;
+    set->end   = NULL;
+    set->pos   = NULL;
+    set->fd    = -1;
+
     if (recording)
     {
-        impl = Btree_Set_create(Allocator_mmap, 4096);
-        if (impl == NULL)
+        /* Create real set instance */
+        set->impl = Btree_Set_create(Allocator_mmap, 4096);
+        if (set->impl == NULL)
         {
-            free(set);
+            set_destroy(set);
+            return NULL;
+        }
+
+        /* Open file */
+        set->fp = fopen(path, recording ? "wb" : "rb");
+        if (set->fp == NULL)
+        {
+            set_destroy(set);
             return NULL;
         }
     }
     else
     {
-        impl = NULL;
-    }
+        struct stat st;
 
-    /* Open file */
-    fp = fopen(path, recording ? "wb" : "rb");
-    if (fp == NULL)
-    {
-        free(set);
-        impl->destroy(impl);
-        return NULL;
+        /* Open file */
+        set->fd = open(path, O_RDONLY);
+        if (set->fd < 0 || fstat(set->fd, &st) < 0)
+        {
+            set_destroy(set);
+            return NULL;
+        }
+
+        /* Memory map file */
+        set->begin = mmap( NULL, (size_t)st.st_size, PROT_READ, MAP_SHARED,
+                           set->fd, (off_t)0 );
+        if (set->begin == NULL)
+        {
+            set_destroy(set);
+            return NULL;
+        }
+        set->end = set->begin + st.st_size;
+        set->pos = set->begin;
     }
 
     set->base.context  = NULL;
@@ -117,8 +152,6 @@ Set *Mock_Set_create(const char *path, bool recording)
     set->base.contains = (void*)set_contains;
     set->base.hash     = default_hash;
     set->base.compare  = default_compare;
-    set->impl = impl;
-    set->fp   = fp;
 
     return &set->base;
 }
